49638e6297aa82f8c4eea4a68000400a15246e5e
panu <panu@qakka.com>
2023-08-09 22:03:21 +0700

    fix setup functional tests

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -240,12 +240,12 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         fname_bitcoind = os.path.join(
             config["environment"]["BUILDDIR"],
             "src",
-            "qtumd" + config["environment"]["EXEEXT"],
+            "firovmd" + config["environment"]["EXEEXT"],
         )
         fname_bitcoincli = os.path.join(
             config["environment"]["BUILDDIR"],
             "src",
-            "qtum-cli" + config["environment"]["EXEEXT"],
+            "firovm-cli" + config["environment"]["EXEEXT"],
         )
         self.options.bitcoind = os.getenv("BITCOIND", default=fname_bitcoind)
         self.options.bitcoincli = os.getenv("BITCOINCLI", default=fname_bitcoincli)
